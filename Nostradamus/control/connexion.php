a58a8e30547d1e4d1d418d9685176ae4adda8568
EISTI Student <eisti@student-laptop.portables.eisti.fr>
2014-08-12 11:34:28 +0200

    connexion updated

--- Nostradamus/control/connexion.php
+++ Nostradamus/control/connexion.php
@@ -1,8 +1,13 @@
+
+
+
 <?php
-    $PARAM_hote="192.168.1.110";
-	$PARAM_nom_bd="nostradamus";
-	$PARAM_utilisateur="sid";
-	$PARAM_mot_passe="sidveloorangetrombone";
+
+// Modify this parameters with your own information
+    $PARAM_hote="hote.xxx";
+	$PARAM_nom_bd="dataBaseName";
+	$PARAM_utilisateur="user";
+	$PARAM_mot_passe="Password";
 
 	$connexion = new PDO("mysql:host=$PARAM_hote;dbname=$PARAM_nom_bd", $PARAM_utilisateur, $PARAM_mot_passe); // connexion à la BDD
 	$connexion->query("set names utf8")
